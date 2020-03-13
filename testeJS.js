7132d838d64ac1230c09451c00c9685bb6810f19
RuanKenned <61585983+RuanKenned@users.noreply.github.com>
2020-03-13 10:24:11 -0300

    a

---
+++ testeJS.js
@@ -0,0 +1,11 @@
+function azul() { 
+    document.getElementById("header").style.backgroundColor = 'blue';
+    //alert(document.getElementById("header").style.backgroundColor);
+    alert("Cabeçalho agora é azul");
+}
+
+function amarelo() { 
+    document.getElementById("header").style.backgroundColor = '#ffb400';
+    //alert(document.getElementById("header").style.backgroundColor);
+    alert("Cabeçalho agora é amerelo topper");
+}
